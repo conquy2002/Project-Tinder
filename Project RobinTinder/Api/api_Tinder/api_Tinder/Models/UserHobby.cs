62d3a903abd11897c68436d9227e527de83ece8a
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-02-14 15:00:12 +0700

    new

--- Project RobinTinder/Api/api_Tinder/api_Tinder/Models/UserHobby.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api_Tinder.Models
-{
-    public class UserHobby
-    {
-        public int ID { get; set; }
-        public int UserId { get; set; }
-        public int InterestID { get; set; }
-    }
-}
--- Project RobinTinder/Api/api_Tinder/api_Tinder/Models/logintoken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api_Tinder.Models
-{
-    public class logintoken
-    {
-        public int Id { get; set; } 
-        public string userID { get; set; }
-        public string token     { get; set; }
-    }
-}
